1de9fe0dbaecb6d52396f2d14483b7274c93261c
Hector Siman <hectorlutero@hotmail.com>
2024-06-08 13:30:35 -0300

    [DOC] Basic Documentation about the project, Get Started, and API routes

--- LearningPlatform/Program.cs
+++ LearningPlatform/Program.cs
@@ -1,3 +1,7 @@
+/// <summary>
+/// The main entry point for the LearningPlatform web application.
+/// This code sets up the web application, configures the services, and runs the application.
+/// </summary>
 using LearningPlatform.Services.Courses;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +14,7 @@ var app = builder.Build();
 {
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
-    
+
     app.MapControllers();
     app.Run();
 }
